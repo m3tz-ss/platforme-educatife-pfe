e81ec7de019eb6de702a616346127b2b6b70490e
m3tz-ss <tarresmoataz840@gmail.com>
2026-03-21 23:30:33 +0100

    Revert "Reapply " modifications des interfaces et code repetitives""
    This reverts commit d21339ca1cd1e419ec61f65341d4d511f6c149dd.

--- front-end-gestion-stage/src/components/layout/index.js
+++
@@ -1,3 +0,0 @@
-export { default as BaseLayout } from "./BaseLayout";
-export { default as AppFooter } from "./AppFooter";
-export { StudentSidebarHeader, EnterpriseSidebarHeader } from "./SidebarHeaders";
